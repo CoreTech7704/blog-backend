1d057681b75520d56486e74a3e76356cbf9fefb3
Sarvam Patel <148036885+CoreTech7704@users.noreply.github.com>
2026-03-14 22:53:46 +0530

    Remove test.js file containing MongoDB connection code

--- test.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb://sarvampatel456_db_user:CoreTech7704@ac-y2wwf6w-shard-00-00.fveolfy.mongodb.net:27017,ac-y2wwf6w-shard-00-01.fveolfy.mongodb.net:27017,ac-y2wwf6w-shard-00-02.fveolfy.mongodb.net:27017/?ssl=true&replicaSet=atlas-cxqtib-shard-0&authSource=admin&appName=blog-cluster")
-.then(()=>console.log("Connected"))
-.catch(err=>console.log(err));
