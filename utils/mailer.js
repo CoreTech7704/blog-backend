f6faa71ff46d902a25a013113f3f07c7b37a9bda
Sarvam Patel <148036885+CoreTech7704@users.noreply.github.com>
2026-03-31 16:53:22 +0530

    Update: streamline email sending logic in sendEmail method

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,18 +1,15 @@
 const { Resend } = require("resend");
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-const test_email = process.env.TEST_EMAIL;
 
 exports.sendEmail = async (to, subject, html) => {
   try {
-    const data = await resend.emails.send({
-      from: "Void Work <onboarding@resend.dev>", 
-      to: test_email || to,
+    await resend.emails.send({
+      from: "Void Work <onboarding@resend.dev>",
+      to: process.env.TEST_EMAIL || to,
       subject,
       html,
     });
-
-    console.log("Email sent:", data);
   } catch (error) {
     console.error("EMAIL ERROR:", error);
     throw error;
